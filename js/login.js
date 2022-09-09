f32ca8e4a648d5a3f288e71be897dcf6869b2976
natasha <natashateresa1998@gmail.com>
2022-09-09 00:56:19 -0300

    Site deestudio de tatuagem

---
+++ js/login.js
@@ -0,0 +1,38 @@
+function login(email, password) {
+  const userStr = localStorage.getItem(email);
+  if (userStr) {
+    const userObj = JSON.parse(userStr);
+
+    const isLogged = userObj.password === password;
+
+    if (isLogged) {
+      return userObj;
+    } else {
+      return null;
+    }
+  }
+
+  return false;
+}
+
+const form = document.getElementById("login-form-id");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // handle the form data
+  console.log(event);
+  const username = form.elements["login-input"].value;
+  const password = form.elements["password-input"].value;
+
+  const loggedUser = login(username, password);
+
+  if (loggedUser) {
+    alert("Usuário autenticado com sucesso");
+    window.location = "/sobre.html";
+  } else {
+    alert("Credenciais incorretas");
+  }
+  console.log({
+    loggedUser,
+  });
+});
